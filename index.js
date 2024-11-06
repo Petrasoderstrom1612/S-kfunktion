763883179edd74f2b655ec7f01da0eb930825d15
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-11-06 17:07:30 +0100

    example with default, commented out. It is shown only for learning purposes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { interplanetaryDestinationsArr as longTrips, shortSpaceTripsArr } from "./data.js"
 import {searchFunction} from "./searchfunction.js"
+// import searchFunction from "./searchfunction.js"  EXAMPLE WITH DEFAULT
 
 
 
--- searchfunction.js
+++ searchfunction.js
@@ -1,5 +1,7 @@
-export const searchFunction = (arr, keyword) => {
+export const searchFunction = (arr, keyword) => { //without export EXAMPLE WITH DEFAULT
     return arr.filter((trip) => {
         return trip.description.includes(keyword)
     })
 }
+
+// export default searchFunction EXAMPLE WITH DEFAULT
